1873f6dd54cba11471c7bf26b4cc16160993459a
김용현 <yhkimjjang@gmail.com>
2024-03-01 23:54:42 +0900

    feat: Timer 적용하여 이미지 롤링 기능 구현

--- WpfImageCarousel.Main/Local/ViewModels/WpfImageCarouselMainViewViewModel.cs
+++ WpfImageCarousel.Main/Local/ViewModels/WpfImageCarouselMainViewViewModel.cs
@@ -1,36 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.ComponentModel;
+using System.Windows.Threading;
 using WpfImageCarousel.Support.Local.Helpers;
 using WpfImageCarousel.Support.Local.Models;
 
 namespace WpfImageCarousel.Main.Local.ViewModels;
 
-public class WpfImageCarouselMainViewViewModel : ObservableObject
+public partial class WpfImageCarouselMainViewViewModel : ObservableObject
 {
-    private ImageInfoManager _imageInfoManger;
-    private string _imagePath;
+    private ImageInfoManager _imageInfoManger;    
+    private DispatcherTimer _timer;
 
-    public event PropertyChangingEventHandler? PropertyChanging;
+    [ObservableProperty]
+    private string imagePath;
 
-    public string ImagePath 
-    {
-        get
-        {
-            return _imagePath;
-        }
-        set
-        {
-            _imagePath = value;
-            OnPropertyChanged(nameof(ImagePath));
-        }
-    }
-
+    /// <summary>
+    /// 생성자
+    /// </summary>
+    /// <param name="imageInfoManger"></param>
     public WpfImageCarouselMainViewViewModel(ImageInfoManager imageInfoManger)
     {
         _imageInfoManger = imageInfoManger;
         InitialImagePath();
+        InitialTimer();
     }
 
+    /// <summary>
+    /// 초기 이미지 정보 조회
+    /// </summary>
     private void InitialImagePath()
     {
         ImageInfo? imageInfo = _imageInfoManger.NextImage();
@@ -40,10 +36,28 @@ public class WpfImageCarouselMainViewViewModel : ObservableObject
         }
     }
 
-    protected virtual void OnPropertyChanged(string propertyName)
+    /// <summary>
+    /// Timer 초기화
+    /// </summary>
+    private void InitialTimer()
     {
-        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(20);
+        _timer.Tick += ChangeImageTimer;
+        _timer.Start();
     }
 
-
+    /// <summary>
+    /// Timer의 인터벌 시간이 되면 이미지를 변경함
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ChangeImageTimer(object? sender, EventArgs e)
+    {
+        ImageInfo? imageInfo = _imageInfoManger.NextImage();
+        if (imageInfo != null)
+        {
+            ImagePath = imageInfo.LocalPath;
+        }
+    }
 }
